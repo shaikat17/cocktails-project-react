d54b2fd532957ae34f9aa78a2434d4d333d1805d
shaikat17 <shaikatpal56@gmail.com>
2023-01-06 15:00:34 +0600

    About and Error Page is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SingleCocktail from "./components/SingleCocktail";
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <h1>new project</h1>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
